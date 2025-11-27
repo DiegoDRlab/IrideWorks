7b8178c712629e6d07ea4507540ea9651ba83073
Diego Ramazzotti <d.ramazzotti@loccioni.com>
2025-11-27 14:21:09 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'IrideWorks',
+  webDir: 'www'
+};
+
+export default config;
